e679072c131ee730b0fadb381a46eec8c8164dea
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-16 21:16:34 +0200

    Connect ViewModel to the window

--- PersonalBudgetManager/MainWindow.xaml.cs
+++ PersonalBudgetManager/MainWindow.xaml.cs
@@ -1,24 +1,17 @@
-﻿using System.Text;
+﻿using PersonalBudgetManager.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PersonalBudgetManager
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public MainViewModel ViewModel { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            ViewModel = new MainViewModel();
+            DataContext = ViewModel;
         }
     }
 }
