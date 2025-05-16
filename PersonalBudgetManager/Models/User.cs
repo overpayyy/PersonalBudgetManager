76188c736352ec87dc007615d8baea6251c6d914
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-16 19:59:39 +0200

    Table user

--- PersonalBudgetManager/Models/User.cs
+++ PersonalBudgetManager/Models/User.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System.Security.Principal;
+
+namespace PersonalBudgetManager.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public List<Account> Accounts { get; set; }
+        public List<Transaction> Transactions { get; set; }
+    }
+}
