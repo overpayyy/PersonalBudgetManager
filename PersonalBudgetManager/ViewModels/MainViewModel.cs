59fa375a07ba2987e0f78110aa8fe037dcf9a27c
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-16 21:09:45 +0200

    When the program starts, it will open the database, load all transactions end save them to the Transactions list to show in the table.

---
+++ PersonalBudgetManager/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using PersonalBudgetManager.Data;
+using PersonalBudgetManager.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace PersonalBudgetManager.ViewModels
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Transaction> Transactions { get; set; }
+
+        public MainViewModel()
+        {
+            LoadTransactions();
+        }
+
+        private void LoadTransactions()
+        {
+            using (var db = new BudgetContext())
+            {
+                var transactionsFromDb = db.Transactions
+                    .OrderByDescending(t => t.Date)
+                    .ToList();
+
+                Transactions = new ObservableCollection<Transaction>(transactionsFromDb);
+            }
+        }
+    }
+}
