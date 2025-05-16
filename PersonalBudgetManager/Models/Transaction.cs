22f3e90322b919f8778ac9ea88f97919f29d6c0f
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-16 20:31:55 +0200

    table transaction

--- PersonalBudgetManager/Models/Transaction.cs
+++ PersonalBudgetManager/Models/Transaction.cs
@@ -1,1 +1,19 @@
-﻿
+﻿namespace PersonalBudgetManager.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
